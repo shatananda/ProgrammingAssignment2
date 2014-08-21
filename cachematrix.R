9e9d825f40d4bd84e25682464bdf5eb21933d435
Noel Shatanada <shatananda@usa.net>
2014-08-21 07:51:16 -0400

    Code added to makeCacheMatrix and cacheSolve
    Code was added to the makeCacheMatrix and the cacheSolve functions to
    support the use of memory variables to reduce processing time.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below intially create the Cache matrix if it does not exist
+## and if it does, it checks to see if the inverse can be pulled from memory
 
-## Write a short comment describing this function
+
+## This function first checks if the matrix exists in memory and then
+## if it is equal to the matrix being supplied. If it is it supplies the 
+## inverse from memory
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ## First check if the variable exists in memory. If not, intialize it with the
+  ## inout matrix and calculate the Inverse
+  if (!exists("CacheMatrix") || is.na(CacheMatrix))
+  {
+    CacheMatrix <<- x
+	InverseMatrix <<- NA
+    InverseMatrix <<- cacheSolve(x)
+  }  
+  ## If the variable exists and is not NA, then check for equality
+  ## If not, then send it down to solve
+  else if (identical(CacheMatrix, x) == FALSE)
+  {
+    InverseMatrix <<- NA
+    retMatrix <- cacheSolve(x)
+  }
+  InverseMatrix  
 }
 
 
-## Write a short comment describing this function
+## This function provides the inverse of the matrix provided. 
+## It checks if the inout provided is already in the cache and 
+## if not re-calculates it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+		if (is.na(InverseMatrix) || (identical(CacheMatrix, x) == FALSE))
+		{
+			InverseMatrix <<- solve(x)
+			CacheMatrix <<- x
+		}
+		## Return a matrix that is the inverse of 'x'
+		InverseMatrix
+        
 }
